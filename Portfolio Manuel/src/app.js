8a932d8f194c6f0f62d52c6ffb89ce7404001460
ManuelRBo <dragonfire069@gmail.com>
2023-11-10 19:00:33 +0100

    Portfolio

--- Portfolio Manuel/src/app.js
+++ Portfolio Manuel/src/app.js
@@ -1,15 +1,31 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const menu = document.getElementById('menu');
 const menuDesplegable = document.getElementById('menu-desplegable');
 const menuDesplegableItems = document.querySelectorAll('.menu-desplegable a');
 const header = document.querySelector('header');
-const contenedorProyectos = document.querySelector('.contenedor-p');
+const contenedorProyectos = document.querySelectorAll('.contenedor-p');
+const enlacesMenu = document.querySelectorAll('.menu-desplegable a');
+const progresoExp = document.querySelectorAll('.progreso');
+
+
+const tl = gsap.timeline();
+tl.from(enlacesMenu[0], {duration: 0.5, opacity:0, x: 100, delay:0.5});
+tl.from(enlacesMenu[1], {duration: 0.5, opacity:0, x: 100}, '-=0.2');
+tl.from(enlacesMenu[2], {duration: 0.5, opacity:0, x: 100}, '-=0.2');
+tl.from(enlacesMenu[3], {duration: 0.5, opacity:0, x: 100}, '-=0.2');
+tl.from(enlacesMenu[4], {duration: 0.5, opacity:0, x: 100}, '-=0.2');
+tl.from(enlacesMenu[5], {duration: 0.5, opacity:0, x: 100}, '-=0.2');
+
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('menu-open');
   menuDesplegable.classList.toggle('menu-desplegable-open');
+  
   if (menuDesplegable.classList.contains('menu-desplegable-open')) {
     header.style.background = 'transparent';
     menuDesplegable.classList.add('cerrar-animacion');
+    tl.restart().play();
   }
 });
 
@@ -20,7 +36,32 @@ menuDesplegableItems.forEach((item) => {
   });
 });
 
-contenedorProyectos.addEventListener('mouseover', (event) => {
-    const proyecto  = event.target.querySelector('.proyectos-hover')
-    console.log(proyecto)
- });
+
+contenedorProyectos.forEach((proyecto) => {
+  proyecto.addEventListener('mouseenter', () => {
+    const proyectos  = proyecto.querySelector('.proyectos-hover');
+    gsap.to(proyectos, {display: 'flex', opacity: 1, duration: 0.5});
+  });
+
+  proyecto.addEventListener('mouseleave', () => {
+    const proyectos  = proyecto.querySelector('.proyectos-hover');
+    gsap.to(proyectos, {display: 'none', opacity: 0, duration: 0.5});
+  });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+progresoExp.forEach((progreso) => {
+  gsap.to(progreso, {
+    scrollTrigger: {
+      trigger: '.contenedor-experiencia',
+      start: 'top 80%',
+      end: '500 80%',
+      toggleActions: 'play reverse play reverse',
+      markers: true,
+    },
+    width: progreso.dataset.width,
+    duration: 1,
+  });
+});
+
